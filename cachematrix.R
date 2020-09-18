adf531d6f521bc6832961c4cd89ef274a82804e8
ruthreshmahadevan <59957968+ruthreshmahadevan@users.noreply.github.com>
2020-09-18 20:13:30 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,46 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
 
-## Caching the Inverse of a Matrix:
-## Matrix inversion is usually a costly computation and there may be some 
-## benefit to caching the inverse of a matrix rather than compute it repeatedly.
-## Below are a pair of functions that are used to create a special object that 
-## stores a matrix and caches its inverse.
-
-## This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-inv <- NULL
-        set <- function(y) {
-                x <<- y
-                inv <<- NULL
-        }
-        get <- function() x
-        setInverse <- function(inverse) inv <<- inverse
-        getInverse <- function() inv
-        list(set = set,
-             get = get,
-             setInverse = setInverse,
-             getInverse = getInverse)
+  i <- NULL # setting i as null which will be a inverse
+  smat <- function(n=matrix()) {  #defining smat function
+    x <<- n   # value of matrix in original place
+    i <<- NULL # if it is new matrix then set the value of inverse as null
+  }
+  gmat <- function() x # will return the argument of matrix
+  sinverse <- function(inv) i <<- inv # assigns value of inverse 
+  ginverse <- function() i # gets the value of inverse
+  list(smat = smat, gmat = gmat, sinverse = sinverse, ginverse=ginverse)
 }
 
+## cacheSolve function 
 
-## Write a short comment describing this function
-
-## This function computes the inverse of the special "matrix" created by 
-## makeCacheMatrix above. If the inverse has already been calculated (and the 
-## matrix has not changed), then it should retrieve the inverse from the cache.
+## This will help to give cached value if not available then it will calculate
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        inv <- x$getInverse()
-        if (!is.null(inv)) {
-                message("getting cached data")
-                return(inv)
-        }
-        mat <- x$get()
-        inv <- solve(mat, ...)
-        x$setInverse(inv)
-        inv
+  i <- x$ginverse() 
+  if(!is.null(i)) {
+    message("Cached value available and return the cached value")
+    return(i)
+  }
+  data <- x$gmat()
+  message("Not available in cache and it will be calculated")
+  i <- solve(data, ...)
+  x$sinverse(i)
+  message("Inverse Matrix")
+  i
 }
